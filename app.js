02985670e23c79216646729a8e82f1e7b6ce7f29
raunak mishra <raunakmishra6300@gmail.com>
2025-03-24 23:22:14 +0530

    add rate limiting & bot protection

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
+app.use(arcjetMiddleware) // Apply Arcjet middleware before the routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware) // Apply Arcjet middleware before the routes
+
 app.listen(PORT, async() => {
     console.log(`Server is running on port ${PORT}`);
     await connectToDatabase();
--- config/env.js
+++ config/env.js
@@ -11,4 +11,6 @@ export const {
     DB_URI,
     JWT_SECRET,
     JWT_EXPIRES_IN,
+    ARCJET_KEY,
+    ARCJET_ENV
 } = process.env;
